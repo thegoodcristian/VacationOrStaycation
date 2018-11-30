abb603f8fd9a2542acc751fda856df72ce58334b
cristian <thegoodcristian@gmail.com>
2018-11-30 10:13:36 -0500

    initial commit

---
+++ src/main/java/com/finalproject/VacationOrStaycation/VacationOrStaycationApplication.java
@@ -0,0 +1,12 @@
+package com.finalproject.VacationOrStaycation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationOrStaycationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationOrStaycationApplication.class, args);
+	}
+}
